2c93061eb28830c53317e597013a4fe03badcd6b
Charles Raj <58942899+charlie-leo@users.noreply.github.com>
2025-07-28 18:42:36 -0400

    deployment commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/health_portfolio/",
   plugins: [
     vue(),
     vueJsx(),
